05d2a7dfb5d53cd92466bf251dbf32a429cb1862
Angel Gomez <angelgomez5975@my.whccd.edu>
2025-02-18 18:24:36 -0800

    Initial commit

---
+++ COMP003A.LectureActivity6-1/Car.cs
@@ -0,0 +1,6 @@
+namespace COMP003A.LectureActivity6_1;
+
+public class Car
+{
+    
+}
---
+++ COMP003A.LectureActivity6-1/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace COMP003A.LectureActivity6_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
